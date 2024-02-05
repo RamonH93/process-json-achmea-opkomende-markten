db399ff96ae6f345f92678f515d22c7f4f642edb
Ramon Houtsma <RamonH93@users.noreply.github.com>
2024-02-05 09:48:43 +0100

    include time in lastUpdated; clean up code

--- main.py
+++ main.py
@@ -4,12 +4,9 @@ import requests
 import sys
 
 REMOTE = "https://www.achmeainvestmentmanagement.nl/Rates/GetAllRatesByFundGroup?code=ODV"
-# REMOTE = "https://tools.morningstar.nl/api/rest.svc/timeseries_price/8qe8f2nger?currencyId=EUR&idtype=Morningstar&frequency=daily&outputType=JSON&startDate=2014-02-01&id=F00001697F]2]0]FONLD$$ALL"
-REMOTE_COPY = "AchmeaOpkomendeMarkten_orig.json"
+# REMOTE_COPY = "AchmeaOpkomendeMarkten_orig.json"
 OUT = "AchmeaOpkomendeMarkten.json"
-# OUT = "MorningStar.json"
 FETCH = True
-DATEFORMAT = '%Y-%m-%d'
 
 data_remote = {}
 
@@ -17,14 +14,14 @@ if FETCH:
     r = requests.get(REMOTE)
     if r.status_code == 200:
         data_remote = r.json()
-        with open(OUT, "w") as f:
-            json.dump(data_remote, f, indent=4)
-else:
-    try:
-        with open(REMOTE_COPY) as f:
-            data_remote = json.load(f)
-    except:
-        pass
+        # with open(REMOTE_COPY, "w") as f:
+        #     json.dump(data_remote, f, indent=4)
+# else:
+#     try:
+#         with open(REMOTE_COPY) as f:
+#             data_remote = json.load(f)
+#     except:
+#         pass
 
 if len(data_remote) == 0:
     print("ERROR: no data.")
@@ -32,18 +29,20 @@ if len(data_remote) == 0:
 
 data_out = {
     'fund': data_remote['funds'][7]['title'],
-    'lastUpdated': datetime.datetime.today().strftime(DATEFORMAT),
+    'lastUpdated': "",
     'rates': []
 }
 
 for item in data_remote['funds'][7]['rates']:
     epoch = item['x'] / 1000
     quote = str(item['y'])
-    date = datetime.datetime.fromtimestamp(epoch).strftime(DATEFORMAT)
+    date = datetime.datetime.fromtimestamp(epoch).strftime('%Y-%m-%d')
     data_out['rates'].append({
         "Date": date,
         "CloseQuote": quote
     })
+
+data_out['lastUpdated'] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
 
 with open(OUT, "w") as f:
     json.dump(data_out, f, indent=4)
